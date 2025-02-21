19273164c5585504a81e2949b70cc44b64254491
pmstudios <dlim@pm-studios.com>
2025-02-21 13:12:13 +0900

    Deploy web app to GitHub Pages

--- Makefile
+++
@@ -1,6 +0,0 @@
-.PHONY: deploy
-
-deploy:
-	@echo "🚀 Deploying to GitHub Pages..."
-	@chmod +x deploy.sh
-	@./deploy.sh 
--- android/app/src/main/kotlin/com/example/oz_text_converter_web/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.oz_text_converter_web
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity()
